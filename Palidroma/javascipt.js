a3aa9c9749d009884fe13e8c4203b527319b5e6d
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-02 13:27:29 +0200

    first commit

---
+++ Palidroma/javascipt.js
@@ -0,0 +1,4 @@
+//Chiedere all'utente di inserire una parola
+const inputUtente = prompt("Inserisci una parola e io vedrò se è palidroma")
+//Creare una funzione per capire se la parola inserita è palindroma
+
