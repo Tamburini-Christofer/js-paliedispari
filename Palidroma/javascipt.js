42639eb1be36ec37eef048f360496bd84dadce4c
christofer.tamburini92@gmail.com <christofer.tamburini92@gmail.com>
2025-09-02 16:22:27 +0200

    Correzione esercizio 1

--- Palidroma/javascipt.js
+++ Palidroma/javascipt.js
@@ -4,14 +4,14 @@ const inputUtente = prompt("Inserisci una parola e io vedrò se è palidroma");
 function verificaPalindromo (input) {
 //Creiamo una variabile che contenga la parola riversata
 //Essa sarà inizialmente vuota.
-    reversedInput = "";
+    let reversedInput = "";
 //Cicliamo l'input con un ciclo For per rebaltare la parola
     for (let i = input.length - 1; i >= 0; i--) {
 //Assegnamo al valore reversedInput il valore delle singole lettere
     reversedInput += input[i];
     }
 //Returniamo il valore alla variabile revertedWord
-    return reversedInput;
+    return reversedInput === input;
 }
 
 //Otteniamo la parola invertita
@@ -19,9 +19,8 @@ const parolaInvertita = verificaPalindromo(inputUtente);
 
 //Creiamo un ciclo if che possa dimostrare per capire se è palindroma e mandare 
 //in console. 
-    if (inputUtente === parolaInvertita) {
+    if (parolaInvertita === true) {
         console.log (`La parola che hai scelto ${inputUtente} è palindroma`)
-    } else if (inputUtente != parolaInvertita) {
+    } else {
         console.log (`La parola che hai scelto ${inputUtente} NON è palindroma`)
     } 
-    
